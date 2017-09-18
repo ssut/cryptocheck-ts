50dee7ba19e5c430277d007313aabf475a9c8e24
SuHun Han <ssut@ssut.me>
2017-09-18 23:07:48 +0900

    Remove unused line

--- routes/index.ts
+++ routes/index.ts
@@ -4,7 +4,6 @@ import * as Koa from 'koa';
 export default function(app: Koa) {
     routesLoader(`${__dirname}`).then((files) => {
         files.forEach((route) => {
-            console.log(route);
             app
                 .use(route.routes())
                 .use(route.allowedMethods({
