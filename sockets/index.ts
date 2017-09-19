834aaf54b28f8c235293690edd2511dcd820047e
SuHun Han <ssut@ssut.me>
2017-09-19 23:01:42 +0900

    Add broadcastMatch feature

--- sockets/index.ts
+++ sockets/index.ts
@@ -5,6 +5,19 @@ import * as actions from './actions';
 
 export interface IWebSocketServer extends WebSocket.Server {
     broadcast(data: any);
+    /**
+     * broadcast if condition matches
+     *
+     * @example <caption>Send "hello!" to clients, which contains "bittrex" in their "subscriptions" context,
+     * in every 5 seconds</caption>
+     * setInterval(() => {
+     *    wss.broadcastMatch((cont): boolean => {
+     *        const subs: Set<string>|undefined = cont.getContext('subscriptions');
+     *        return subs && subs.has('bittrex');
+     *    }, 'hello!');
+     * }, 5000);
+     */
+    broadcastMatch(cond: (cont: SocketDataController) => boolean, data: any);
 }
 
 export interface IWebSocketConnection extends WebSocket {
@@ -18,14 +31,22 @@ const handling = (wss: IWebSocketServer) => {
         ws.controller = new SocketDataController(wss, ws, request);
     };
 
-    wss.broadcast = function broadcast(data) {
-        wss.clients.forEach(function each(client) {
+    wss.broadcast = (data: any) => {
+        wss.clients.forEach((client) => {
             if (client.readyState === WebSocket.OPEN) {
                 client.send(data);
             }
         });
     };
+    wss.broadcastMatch = (cond: (cont: SocketDataController) => boolean, data: any) => {
+        wss.clients.forEach((client: IWebSocketConnection) => {
+            if (client.readyState === WebSocket.OPEN && client.controller && cond(client.controller)) {
+                client.send(data);
+            }
+        });
+    };
     wss.on('connection', handleConnection);
+
 };
 
 export default handling;
